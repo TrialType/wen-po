9837ba43f95a85513ff8c2aef56874a1ee627a28
xie100 <2948049467@qq.com>
2024-05-29 23:57:25 +0800

    2

--- src/wen/po.java
+++ src/wen/po.java
@@ -2,26 +2,17 @@ package wen;
 
 import arc.Core;
 import arc.Events;
-import arc.files.Fi;
 import arc.util.Time;
 import mindustry.Vars;
-import mindustry.content.Blocks;
-import mindustry.content.TechTree;
 import mindustry.ctype.ContentType;
 import mindustry.ctype.UnlockableContent;
 import mindustry.game.EventType;
 import mindustry.mod.*;
-import mindustry.ui.dialogs.BaseDialog;
 import wen.WContents.TechTrees;
 import wen.WContents.WBlocks;
 import wen.WContents.WPlanets;
 import wen.WType.MoreTechResearchDialog;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-import static mindustry.Vars.content;
 import static mindustry.Vars.mods;
 
 public class po extends Mod {
@@ -30,14 +21,13 @@ public class po extends Mod {
         WBlocks.load();
         WPlanets.load();
         TechTrees.load();
-
         Events.on(EventType.ClientLoadEvent.class, e -> Time.runTask(10f, () -> Vars.ui.research = new MoreTechResearchDialog()));
         Events.on(EventType.ContentInitEvent.class, e -> rebuildFromFile());
     }
 
     public void rebuildFromFile() {
         for (Mods.LoadedMod mod : mods.orderedMods()) {
-            if (mod.name.contains("wen") && mod.name.contains("po")) {
+            if (mod.name.contains("wen-po")) {
                 if (Core.bundle.get("@GEOTech1") != null) {
                     UnlockableContent content = null;
                     String name = Core.bundle.get("@GEOTech1");
@@ -50,9 +40,10 @@ public class po extends Mod {
                     if (content != null) {
                         TechTrees.geo_node1.content = content;
                     }
-                } else if (Core.bundle.get("@GEOTech2") != null) {
+                }
+                if (Core.bundle.get("@GEOTech2") != null) {
                     UnlockableContent content = null;
-                    String name = Core.bundle.get("@GEOTech1");
+                    String name = Core.bundle.get("@GEOTech2");
                     for (ContentType type : ContentType.all) {
                         content = Vars.content.getByName(type, name);
                         if (content != null) {
